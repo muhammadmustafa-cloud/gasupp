871d5633b4f68e8a2125ed8b9dce99faae818a83
Mustafa Memon <mustafamemon360@gmail.com>
2024-09-23 02:24:55 +0500

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ServicePage from './components/servicePage'
 import ContactUsPage from './components/contactUsPage'
+import WhyChooseUs from './components/whyChooseUs'
 function App() {
 
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/about-us' element={<AboutPage/>} />
           <Route path='/service' element={<ServicePage/>}/>
           <Route path='/contact-us' element={<ContactUsPage/>}/>
+          <Route path='/why-choose-us' element={<WhyChooseUs/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
-import { Link } from 'react-router-dom'
-import logo from '../assets/logo.png'
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
+
 const Navbar = () => {
   return (
     <div className='flex bg-white justify-around items-center'>
@@ -8,18 +9,43 @@ const Navbar = () => {
       </div>
       <div>
         <ul className='flex gap-10 font-medium'>
-          <Link to='/'><li className='text-[#41CB5B]'>Home</li></Link>
-          <Link to='/about-us'><li>About</li></Link>
-          <Link to='/service'><li>Services</li></Link>
-          <Link to='/contact-us'><li>Contact Us</li></Link>
-          <li>About Us</li>
+          <NavLink 
+            to='/' 
+            className={({ isActive }) => isActive ? 'text-[#41CB5B]' : ''}
+          >
+            <li>Home</li>
+          </NavLink>
+          <NavLink 
+            to='/about-us' 
+            className={({ isActive }) => isActive ? 'text-[#41CB5B]' : ''}
+          >
+            <li>About</li>
+          </NavLink>
+          <NavLink 
+            to='/service' 
+            className={({ isActive }) => isActive ? 'text-[#41CB5B]' : ''}
+          >
+            <li>Services</li>
+          </NavLink>
+          <NavLink 
+            to='/contact-us' 
+            className={({ isActive }) => isActive ? 'text-[#41CB5B]' : ''}
+          >
+            <li>Contact Us</li>
+          </NavLink>
+          <NavLink 
+            to='/why-choose-us' 
+            className={({ isActive }) => isActive ? 'text-[#41CB5B]' : ''}
+          >
+            <li>Why Choose Us</li>
+          </NavLink>
         </ul>
       </div>
       <div className='greenColor text-white font-medium px-6 py-4 rounded-full cursor-pointer'>
         <button>Download App</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
